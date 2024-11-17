3d1cece6ecc766cd241520cb8e25fccfdbe3d452
Fridman Baruch <moshe.baruch@post.runi.ac.il>
2024-11-17 20:20:16 +0200

    timecalc primary

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,19 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        String time = args[0];
+        int hours = 0, minutes = 0;
+        //transfer string digits to int variables:
+        hours = (Integer.parseInt(time.charAt(0) + "") * 10 + Integer.parseInt(time.charAt(1) + ""));
+        minutes = (Integer.parseInt(time.charAt(3) + "") * 10 + Integer.parseInt(time.charAt(4) + ""));
+
+        int newTime = hours * 60 + minutes;
+        int addedTime = Integer.parseInt(args [1]);
+        //recalculate time:
+        newTime = newTime + addedTime;
+        minutes = newTime % 60;
+        hours = newTime / 60;
+
+        System.out.printf("%02d:%02d", hours, minutes);
+
     }
 }
