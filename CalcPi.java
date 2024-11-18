e21a3e7da89f8350ebb94a65b2073619eb5db3af
Fridman Baruch <moshe.baruch@post.runi.ac.il>
2024-11-18 15:52:34 +0200

    calcpi primary

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,19 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+		int range = Integer.parseInt(args[0]);
+		double sum = 0;
+		boolean minus = false;
+		for (int i = 1; i < 2 * range; i = i +2){
+			if (minus) {
+				sum = sum - (1.0 / i);
+			}else{
+				sum = sum + (1.0 / i);
+			}
+			minus = !minus;
+		}
+		sum = sum * 4;
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated:     " + sum);
 	}
 }
