55646874d338fae23938bcd4acc3d1ee310f77bd
Fridman Baruch <moshe.baruch@post.runi.ac.il>
2024-11-18 17:36:52 +0200

    collatz primary

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,25 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		int length = Integer.parseInt(args[0]);
+		String mode = args[1];
+
+		if(mode.equals("v")){
+			for(int i = 1; i - 1 < length; i++){
+				int count = 1, current = i;
+				while (current != 1){
+					System.out.print(current + " ");
+					if (current % 2 == 0){
+						current = current / 2;
+					}else{
+						current = current * 3 + 1;
+					}
+					count++;
+				}System.out.println("1 (" + count + ")");
+			}
+		}else if(mode.equals("c")){
+			System.out.println("Every one of the first " + length + " hailstone sequences reached 1.");
+
+		}
 	}
 }
